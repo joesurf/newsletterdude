81eca233c3f1eb6073896f4ec6218e05f391e22e
joesurf <desictiverk@gmail.com>
2023-08-23 21:00:17 +0800

    Fix header responsiveness using menu items

--- src/components/layout/General/Header/Header.js
+++ src/components/layout/General/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import './Header.css';
 import { Paper, Box, IconButton, Link, Toolbar, Typography, useTheme } from '@mui/material';
 import { ColorModeContext, tokens } from '../../../../theme';
@@ -6,6 +6,7 @@ import logo from '../../../../assets/NewsletterDudeLogo.png';
 
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import MenuIcon from '@mui/icons-material/Menu';
 
 
 function Header() {
@@ -14,29 +15,57 @@ function Header() {
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth)
+    } 
+
+    window.addEventListener('resize', handleResize)
+  }, [])
+
+  const toggleMenu = () => {
+    console.log("open/close")
+  }
+
   return (
     <Paper>
     <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-      <Box display="flex">
+        <Box display="flex" alignItems="center">
+          <Link href="/">
+              <img src={ logo } alt="logo" style={{ height: "40px", paddingLeft: "5px" }}></img>
+          </Link>
+        </Box>
         <Toolbar>
-            <Link href="/">
-                <img src={ logo } alt="logo" style={{ height: "40px", paddingRight: "20px" }}></img>
-            </Link>
-            <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-                <Link href='/' underline="none" sx={{ padding: "15px", paddingLeft: "0px", color: colors.grey[100] }}>NewsletterDude</Link>
-                <Link href='/blog' underline="none" sx={{ padding: "15px", color: colors.grey[100] }}>Blog</Link>
-                <Link href='/tools' underline="none" sx={{ padding: "15px", color: colors.grey[100] }}>Tools</Link>
-            </Typography>
+          {windowWidth < 450 
+            ? 
+            <Box display="flex">
+              <IconButton onClick={toggleMenu}>
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            
+            : (
+            <Box display="flex">
+              <Link href='/' underline="none" sx={{ padding: "15px", color: colors.grey[100] }}>
+                <Typography variant="h4" sx={{ flexGrow: 1 }}>
+                  NewsletterDude
+                </Typography>
+              </Link>
+              <Link href='/blog' underline="none" sx={{ padding: "15px", color: colors.grey[100] }}>
+                <Typography variant="h4" sx={{ flexGrow: 1 }}>
+                  Blog
+                </Typography>
+              </Link>
+              <Link href='/tools' underline="none">
+                <Typography variant="h4" sx={{ flexGrow: 1, padding: "15px", color: colors.grey[100] }}>
+                  Tools
+                </Typography>
+              </Link>
+            </Box>
+          )}
         </Toolbar>
-        {/* <Link href="/">
-            <img src={ logo } alt="logo" style={{ height: "40px", paddingRight: "20px" }}></img>
-        </Link>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link href='/' underline="none" sx={{ padding: "15px", paddingLeft: "0px", color: 'black' }}>NewsletterDude</Link>
-            <Link href='/blog' underline="none" sx={{ padding: "15px", color: 'black' }}>Blog</Link>
-            <Link href='/tools' underline="none" sx={{ padding: "15px", color: 'black' }}>Tools</Link>
-        </Typography> */}
-      </Box>
       <Box display="flex">
         <IconButton onClick={colorMode.toggleColorMode}>
           {theme.palette.mode === 'dark'
