7f7dbaf1193e8054dec2d949b567714b18be7cb4
joesurf <desictiverk@gmail.com>
2023-08-26 14:43:07 +0800

    Update community page into a waitlist

--- src/components/layout/CommunityPage/CommunityJoinButton.js
+++ src/components/layout/CommunityPage/CommunityJoinButton.js
@@ -11,11 +11,11 @@ export default function CommunityJoinButton() {
     return (
         <Box>
             <HashLink>
-                <Button 
-                    onClick={() => {window.open("https://discord.gg/ZnnqEfrC", "_blank")}} 
+                <Button
+                    onClick={() => {window.open("https://blog.newsletterdude.com/#/portal/signup/free", "_blank")}} 
                     sx={{ backgroundColor: colors.grey[100], color: colors.grey[900] }}
                 >
-                    Join the newsletter community
+                    Join the newsletter community waitlist
                 </Button>
             </HashLink>
         </Box>
--- src/components/layout/CommunityPage/HeroBanner.js
+++ src/components/layout/CommunityPage/HeroBanner.js
@@ -1,20 +1,20 @@
 import "./HeroBanner.css";
 
 import { Typography, Box } from "@mui/material";
-import RatingAvatar from "../../UI/RatingAvatar/RatingAvatar";
+// import RatingAvatar from "../../UI/RatingAvatar/RatingAvatar";
 import CommunityJoinButton from "./CommunityJoinButton";
 
 
-const TESTIMONIALS = [
-  {
-    name: "Joseph Long",
-    rating: 4.5,
-  },
-  {
-    name: "Nicholas Fo",
-    rating: 4.2,
-  },
-]
+// const TESTIMONIALS = [
+//   {
+//     name: "Joseph Long",
+//     rating: 4.5,
+//   },
+//   {
+//     name: "Nicholas Fo",
+//     rating: 4.2,
+//   },
+// ]
 
 
 const HeroBanner = (props) => {
@@ -44,11 +44,21 @@ const HeroBanner = (props) => {
         variant="h4"
         sx={{ my: "2rem", maxWidth: "1000px", mx: "auto" }}
       >
-          Our community is an online mastermind community for newsletter builders looking 
-          to participate in deep dive discussions on building their newsletters.
+          An online mastermind community for newsletter builders looking 
+          to participate in deep dive discussions on building their newsletters. 
       </Typography>
       <CommunityJoinButton />
-      <RatingAvatar testimonials={TESTIMONIALS} />
+      {/* <RatingAvatar testimonials={TESTIMONIALS} /> */}
+      <Typography
+        variant="h5"
+        sx={{ my: "2rem", maxWidth: "1000px", mx: "auto" }}
+      >
+          <i>
+            All paid subscribers will be invited to the community once opened. 
+            Non-subscribers will have to create a free account on the blog to 
+            join the waitlist.
+          </i> 
+      </Typography>
 
     </Box>
   );
