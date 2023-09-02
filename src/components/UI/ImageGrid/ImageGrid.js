f12c8bc79c7015d49181a2f711099740dff93118
joesurf <desictiverk@gmail.com>
2023-09-02 08:59:01 +0800

    Fix list card responsiveness

--- src/components/UI/ImageGrid/ImageGrid.js
+++ src/components/UI/ImageGrid/ImageGrid.js
@@ -5,7 +5,7 @@ import { Grid } from "@mui/material";
 const ImageGrid = (props) => {
   return (
     <Grid item lg={props.gridSize} className={css.imgContainer}>
-      <img src={props.src} alt={props.alt} style={{ maxWidth: props.maxWidth }} />
+      <img src={props.src} alt={props.alt} style={{ maxWidth: props.maxWidth, width: props.width }} />
     </Grid>
   );
 };
--- src/components/layout/RoadmapPage/ListCard.js
+++ src/components/layout/RoadmapPage/ListCard.js
@@ -63,15 +63,23 @@ function ListCard({ case_study }) {
                     alignItems="center" 
                     justifyContent={windowWidth > RESPONSIVE_WIDTH ? "" : "center"}
                 >
-                    {case_study.image_id 
-                    ? <ImageGrid src={`${process.env.REACT_APP_SUPABASE_URL}/storage/v1/object/public/NewsletterDude/${case_study.image_id}`} maxWidth="50px" />
-                    : <QuestionMarkIcon />}
-                    <Typography variant="h4" sx={{ color: colors.grey[100], mx: "10px", textAlign: "left" }}>
-                        {case_study.title}
-                        <Tooltip title="Values are estimated. Click to visit the website." placement="top-end">
-                            <InfoOutlinedIcon fontSize="small" onClick={() => { window.open(case_study.website_link, '_blank', 'noreferrer') }} />
-                        </Tooltip>
-                    </Typography>
+                    <Box width="20%">
+                        {case_study.image_id 
+                        ? <ImageGrid 
+                            src={`${process.env.REACT_APP_SUPABASE_URL}/storage/v1/object/public/NewsletterDude/${case_study.image_id}`} 
+                            maxWidth="50px"
+                            width="40px"
+                        />
+                        : <QuestionMarkIcon fontSize="large" />}
+                    </Box>
+                    <Box width="80%">
+                        <Typography variant="h4" sx={{ color: colors.grey[100], mx: "10px", textAlign: "left" }}>
+                            {case_study.title}
+                            <Tooltip title="Values are estimated. Click to visit the website." placement="top-end">
+                                <InfoOutlinedIcon fontSize="small" onClick={() => { window.open(case_study.website_link, '_blank', 'noreferrer') }} />
+                            </Tooltip>
+                        </Typography>
+                    </Box>
                 </Box>
                 {windowWidth > RESPONSIVE_WIDTH ? <></> : <Divider variant="middle" sx={{ my: "10px" }} />}
                 <Box width={windowWidth > RESPONSIVE_WIDTH ? "60%" : "100%"} display="flex" flexDirection="row" justifyContent="space-around">
